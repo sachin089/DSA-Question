78e0249996b61c33c2dd3f7724265ac64741a3a7
sachin31344 <105095798+sachin31344@users.noreply.github.com>
2023-06-29 11:29:32 +0530

    Strings

---
+++ Strings/Isomorphic_String.cpp
@@ -0,0 +1,23 @@
+// testCase
+// Input: s = "egg", t = "add"
+// Output: true
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int hash[256]={0};
+        bool isMap[256]={0};
+        for(int i=0;i<s.size();i++){
+            if(hash[s[i]] == 0 && isMap[t[i]]==0){
+                hash[s[i]]=t[i];
+                isMap[t[i]]=true;
+            }
+        }
+
+        for(int i=0;i<s.size();i++){
+            if(char(hash[s[i]]) != t[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
